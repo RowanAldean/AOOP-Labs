c3aeb6a9eaf4b7d31e4956106ffb7edba65bda2c
RowanAldean <rowanaldean@gmail.com>
2022-03-04 02:05:12 +0000

    Lab6Task1 maybe complete and PDF attached

---
+++ Lab6/cat.cpp
@@ -0,0 +1,41 @@
+#include "cat.h"
+#include <iostream>
+
+Cat::Cat(): name("Tom"), lives(9){
+     std::cout << "Default constructor called... \n";
+};
+
+Cat::Cat(std::string name): name(name), lives(9){
+     std::cout << "Constructor called with name... \n";
+};
+
+Cat::Cat(std::string name, unsigned int lives) : name(name), lives(lives){
+    std::cout << "Constructor called with name and lives... \n";
+};
+
+Cat::~Cat(){
+    std::cout << "Destructor called... \n";
+}
+
+void Cat::setName(std::string const name){
+    std::cout << "setName called... \n";
+    this->name = name;
+}
+
+void Cat::setLives(unsigned int const lives){
+    std::cout << "setLives called... \n";
+    if(lives >= this->lives){
+        return;
+    }
+    else if ((this->lives > 0) && (lives == 0))
+    {
+        setName("The Cat formerly known as " + getName());
+    }
+    
+    this->lives = lives;
+}
+
+std::ostream &operator<<(std::ostream &os, const Cat& cat){
+    os << cat.name << cat.lives;
+    return os;
+}
---
+++ Lab6/cat.h
@@ -0,0 +1,52 @@
+#ifndef CAT_H
+#define CAT_H
+#include <string>
+#include <sstream>
+#include <functional>
+
+class Cat
+{
+private:
+    std::string name;
+    unsigned int lives;
+
+public:
+    Cat();
+    Cat(std::string name);
+    Cat(std::string name, unsigned int lives);
+    ~Cat();
+
+    std::string getName() const
+    {
+        return name;
+    }
+
+    unsigned int getLives() const
+    {
+        return lives;
+    }
+
+    void setName(std::string const name);
+
+    void setLives(unsigned int const lives);
+
+    friend std::ostream &operator<<(std::ostream &os, const Cat &cat);
+};
+
+namespace std
+{
+    template <>
+    struct hash<Cat>
+    {
+        size_t operator()(const Cat &obj) const
+        {
+            std::stringstream stream;
+            stream << obj;
+            size_t output = std::hash<std::string>{}(stream.str());
+            // integer and return it...
+            return output;
+        }
+    };
+}
+
+#endif
