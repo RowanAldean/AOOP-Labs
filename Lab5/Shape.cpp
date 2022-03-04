173214ef1971cd50c6e22e9b063a4d4833f00f04
RowanAldean <rowanaldean@gmail.com>
2022-03-04 00:03:31 +0000

    Task3 Shapes complete

--- Lab5/Shape.cpp
+++ Lab5/Shape.cpp
@@ -65,3 +65,60 @@ void Rectangle::centre_at(double x, double y)
 
 // Triangle functions
 
+Triangle::Triangle(double x0, double y0, double x1, double y1, double x2, double y2) : x0(x0), y0(y0), x1(x1),
+                                                                                       y1(y1), x2(x2), y2(y2), Shape()
+{
+    std::cout << "Triangle constructed with 6 doubles... \n";
+};
+
+Triangle::~Triangle(){};
+
+std::string Triangle::to_string() const
+{
+
+    std::stringstream stream;
+    stream << "Triangle at [(" << this->x0 << "," << this->y0 << "), "
+           << "(" << this->x1 << "," << this->y1 << "), "
+           << "(" << this->x2 << "," << this->y2 << ")] ";
+    stream << "with side lengths "
+           << get_side(this->x2, this->y2, this->x0, this->y0) << ", "
+           << get_side(this->x1, this->y1, this->x2, this->y2) << ", and "
+           << get_side(this->x0, this->y0, this->x1, this->y1);
+    std::string str = stream.str();
+    return str;
+};
+
+void Triangle::centre_at(double x, double y)
+{
+    double translated_x = fabs(((this->x0 + this->x1 + this->x2) / 3) - x);
+    double translated_y = fabs(((this->y0 + this->y1 + this->y2) / 3) - y);
+
+    this->x0 += translated_x;
+    this->x1 += translated_x;
+    this->x2 += translated_x;
+
+    this->y0 += translated_y;
+    this->y1 += translated_y;
+    this->y2 += translated_y;
+
+    //The below was for testing accuracy - fabs or std::abs solved rounding problems
+
+    // double centroid_x = ((this->x0 + this->x1 + this->x2) / 3);
+    // double centroid_y = ((this->y0 + this->y1 + this->y2) / 3);
+    // std::cout << "Centroid is now: " << "(" << centroid_x << "," << centroid_y << ")" << std::endl;
+};
+
+double Triangle::get_side(double xp0, double yp0, double xp1, double yp1) const
+{
+    /* We do pythagorus with the following to find their lengths,
+    it's just using the 2 points to find a respective their point made up
+    of their x and y's then finding their length via that.
+    */
+    // x0,y0 = A, x1,y0 = B, x1,y1 = C LINE A
+
+    // x1,y1 = A, x2,y1 = B, x2,y2 = C LINE B
+
+    // x2,y2 = A, x0,y2 = B, x0,y0 = C LINE C
+    return sqrt(pow(fabs(yp0 - yp1), 2) +
+                pow(fabs(xp0 - xp1), 2));
+};
--- Lab5/Shape.h
+++ Lab5/Shape.h
@@ -38,5 +38,19 @@ public:
     virtual void centre_at(double x, double y);
 };
 
+struct Triangle : public Shape
+{
+private:
+    double x0, y0, x1, y1, x2, y2;
+
+public:
+    Triangle(double x0, double y0, double x1, double y1, double x2, double y2);
+    virtual ~Triangle();
+
+    virtual std::string to_string() const;
+    virtual void centre_at(double x, double y);
+    double get_side(double xp0, double yp0, double xp1, double yp1) const;
+    void get_centroid();
+};
 
 #endif
