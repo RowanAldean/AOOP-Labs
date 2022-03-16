a2b6cc468d09af71032b3aa4b2c66f2ab7b48b60
RowanAldean <rowanaldean@gmail.com>
2022-03-16 13:58:32 +0000

    Test 2 passed using string based design and custom exception inheriting from out_of_range.

--- Coursework/src/item.cpp
+++ Coursework/src/item.cpp
@@ -14,6 +14,7 @@
 //
 // Example:
 //  Item iObj{"identIdent"};
+Item::Item(std::string ident) : identifier(ident), entries(){};
 
 // TODO Write a function, size, that takes no parameters and returns an unsigned
 //  int of the number of entries in the Item contains.
@@ -21,6 +22,10 @@
 // Example:
 //  Item iObj{"identIdent"};
 //  auto size = iObj.size();
+unsigned int Item::size()
+{
+    return entries.size();
+};
 
 // TODO Write a function, empty, that takes no parameters and returns true
 //  if the number of entries in the Item is zero, false otherwise.
@@ -28,20 +33,29 @@
 // Example:
 //  Item iObj{"identIdent"};
 //  auto empty = iObj.empty();
-
+bool Item::empty()
+{
+    return (entries.size() == 0 ? true : false);
+};
 // TODO Write a function, setIdent, that takes one parameter, a string for a new
 //  Item identifier, and updates the member variable. It returns nothing.
 //
 // Example:
 //  Item iObj{"identIdent"};
 //  iObj.setIdent("identIdent2");
-
+void Item::setIdent(std::string newIdent)
+{
+    identifier = newIdent;
+}
 // TODO Write a function, getIdent, that returns the identifier for the Item.
 //
 // Example:
 //  Item iObj{"identIdent"};
 //  auto ident = iObj.getIdent();
-
+std::string Item::getIdent()
+{
+    return identifier;
+};
 // TODO Write a function, addEntry, that takes two parameters, an entry
 //  key and value and returns true if the entry was inserted into the
 //  container or false if the entry already existed and was replaced.
@@ -49,7 +63,21 @@
 // Example:
 //  Item iObj{"identIdent"};
 //  iObj.addEntry("key", "value");
-
+bool Item::addEntry(std::string key, std::string value)
+{
+    std::map<std::string, std::string>::iterator it = entries.find(key);
+    if (it == entries.end())
+    {
+        //not found
+        entries.insert(std::pair<std::string, std::string>(key, value));
+        return true;
+    }
+    else
+    {
+        it->second = value;
+        return false;
+    }
+}
 // TODO Write a function, getEntry, that takes one parameter, an entry
 //  key and returns it's value. If no entry exists, throw an appropriate
 //  exception.
@@ -58,7 +86,19 @@
 //  Item iObj{"identIdent"};
 //  iObj.addEntry("key", "value");
 //  auto value = iObj.getEntry("key");
-
+std::string Item::getEntry(std::string keyToGet)
+{
+    std::map<std::string, std::string>::iterator it = entries.find(keyToGet);
+    if (it != entries.end())
+    {
+        //found
+        return it->second;
+    }
+    else
+    {
+        throw EntryDoesntExistException(keyToGet, identifier);
+    }
+}
 // TODO Write a function, deleteEntry, that takes one parameter, an entry
 //  key, deletes it from the container, and returns true if the Item was
 //  deleted. If no entry exists, throw an appropriate exception.
@@ -67,7 +107,20 @@
 //  Item iObj{"identIdent"};
 //  iObj.addEntry("key", "value");
 //  iObj.deleteEntry("key");
-
+bool Item::deleteEntry(std::string keyToDelete)
+{
+    std::map<std::string, std::string>::iterator it = entries.find(keyToDelete);
+    if (it != entries.end())
+    {
+        //found
+        entries.erase(it);
+        return true;
+    }
+    else
+    {
+        throw EntryDoesntExistException(keyToDelete, identifier);
+    }
+}
 // TODO Write an == operator overload for the Item class, such that two
 //  Item objects are equal only if they have the same identifier and same
 //  entries.
@@ -79,6 +132,12 @@
 //  if(iObj1 == iObj2) {
 //    ...
 //  }
+bool operator==(const Item& lhs, const Item& rhs){
+    if((lhs.identifier == rhs.identifier) && (lhs.entries == rhs.entries)){
+        return true;
+    }
+    return false;
+}
 
 // TODO Write a function, str, that takes no parameters and returns a
 //  std::string of the JSON representation of the data in the Item.
@@ -88,3 +147,7 @@
 // Example:
 //  Item iObj{"itemIdent"};
 //  std::string s = iObj.str();
+std::string Item::str(){
+    json j_map(entries);
+    return j_map.dump();
+}
--- Coursework/src/item.h
+++ Coursework/src/item.h
@@ -16,8 +16,63 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-class Item {
+#include <iostream>
+#include <string>
+#include <map>
+#include "lib_json.hpp"
+using json = nlohmann::json;
 
+//TODO: Use generics for everything instead of just string?
+class Item
+{
+private:
+    std::string identifier;
+    std::map<std::string, std::string> entries;
+
+public:
+    Item(std::string identifier);
+
+    /*We are returning by value (copy) and so const isn't needed as 
+    the caller can do as they please with their copy without
+    impacting our entry data.*/
+    std::string getEntry(std::string const key);
+
+    //Return the length of the entries map.
+    unsigned int size();
+
+    //Checks if entries are empty
+    bool empty();
+
+    void setIdent(std::string newIdentifier);
+
+    std::string getIdent();
+
+    bool addEntry(const std::string key, const std::string value);
+
+    bool deleteEntry(const std::string key);
+
+    friend bool operator==(const Item &lhs, const Item &rhs);
+    
+    std::string str();
+};
+
+class EntryDoesntExistException : public std::out_of_range
+{
+private:
+    std::string entryName;
+    std::string itemName;
+
+public:
+    EntryDoesntExistException(std::string entryName, std::string itemName) : out_of_range("Entry key is out of range"), entryName(entryName),
+                                                                             itemName(itemName) {}
+
+    virtual void describe(std::ostream &os) const
+    {
+        os << "The following entry does not exist:"
+           << "\n"
+           << "Item: " << itemName
+           << "Entry key: " << entryName;
+    }
 };
 
 #endif // ITEM_H
